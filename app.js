af59544b321fd8e88f5ac80594d8dc67f64c9895
saiteja527 <boddepalli.ramana48@gmail.com>
2024-05-12 19:22:54 +0530

    save

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ inputBox.addEventListener("keypress", handleKeyPress);
 taskItems.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData();
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
